277683d66df6e9cd00f6839e828a056a41cca932
tanvirtin <tanvir.tinz@gmail.com>
2017-09-20 21:30:41 -0400

    server and react setup complete

--- app/index.android.js
+++ app/index.android.js
@@ -1,53 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Main from "./components/Main/Main"
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+	AppRegistry,
+} from "react-native";
 
-export default class Ryde extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+class Ryde extends Component {
+	render() {
+		return (
+			<Main/>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+module.exports = Ryde;
 
-AppRegistry.registerComponent('Ryde', () => Ryde);
+AppRegistry.registerComponent("Ryde", () => Ryde);
