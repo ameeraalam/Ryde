be652af295cf96ddc6596a0b4057f4312b699b91
tanvirtin <tanvir.tinz@gmail.com>
2017-10-25 17:14:40 -0400

    added a few styles

--- app/components/Main/Main.js
+++ app/components/Main/Main.js
@@ -11,11 +11,11 @@ import Chat from "../Chat/Chat";
 export default () => (
 	<Router navigationBarStyle={styles.navbarContainer} titleStyle={styles.navbarTitle}>
 		<Scene key = "root">
-			<Scene key = "chat" component = {Chat} title = "Ryde"/>		
-			<Scene key = "login" component = {Login} title = "Ryde"/>
+			<Scene key = "login" component = {Login} title = "Ryde"/>	
+			<Scene key = "driverInfo" component = {DriverInfo} title = "Ryde"/>
 			<Scene key = "register" component = {Register} title = "Ryde"/>
 			<Scene key = "choice" component = {Choice} title = "Ryde"/>	
-			<Scene key = "driverInfo" component = {DriverInfo} title = "Ryde"/>
+			<Scene key = "chat" component = {Chat} title = "Ryde"/>	
 		</Scene>
 	</Router>
 );
